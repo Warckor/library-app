c533992c8e7cb49435ed63342d1ad9fa0270b74b
Warckor <mik.el.loco@gmail.com>
2023-09-24 00:12:56 +0200

    Add show dialogBox modal

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const bookList = document.querySelector('.books');
-const addNewBook = document.querySelector('.add-new-book');
-const addButton = document.querySelector('.add');
-const modal = document.querySelector('.modal');
+const bookList = document.querySelector('.books')
+const addNewBook = document.querySelector('.add-new-book')
+const addButton = document.querySelector('.add')
+const dialogBox = document.querySelector('.dialog-box')
 
 const Book = (title, author, pages, read) => {
   return {
@@ -11,3 +11,13 @@ const Book = (title, author, pages, read) => {
     read
   }
 }
+
+const addBook = (title, author, pages, read) => {
+  const book = new Book(title, author, pages, read)
+  return book
+}
+
+addNewBook.addEventListener('click', () => {
+  console.log('clicked')
+  dialogBox.showModal()
+})
